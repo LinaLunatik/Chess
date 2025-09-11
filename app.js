5577a8ee070854bcd128b639d76af5965ef2ee27
LinaLunatik <lina.rudnewa@yandex.ru>
2025-09-11 16:03:17 +0300

    Подсветка любой выбранной фигуры
    При выборе любой фигуры фон окрашивается зеленым

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-const rook = document.querySelector('#rook')
+const rooks = document.querySelectorAll('.rook')
+const knights = document.querySelectorAll('.knight')
+const bishops = document.querySelectorAll('.bishop')
+const queens = document.querySelectorAll('.queen')
+const kings = document.querySelectorAll('.king')
+const pawns = document.querySelectorAll('.pawn')
 
-rook.addEventListener('click', possibleStep)
+rooks.forEach(rook => {rook.addEventListener('click', possibleStepRook)}) 
+knights.forEach(knight => {knight.addEventListener('click', possibleStepKnight)})
+bishops.forEach(bishop => {bishop.addEventListener('click', possibleStepBishop)})
+queens.forEach(queen => {queen.addEventListener('click', possibleStepQueen)})
+kings.forEach(king => {king.addEventListener('click', possibleStepRookKing)})
+pawns.forEach(pawn => {pawn.addEventListener('click', possibleStepPawn)})
 
-function possibleStep(event) {
+function selectFigure(event) {
     const cell = event.target.parentElement;
-    cell.style.background = '#b5daa4ff'
+    cell.classList.toggle('selectedItem')
+}
+
+function possibleStepRook(event) {
+    selectFigure(event)
+}
+
+function possibleStepKnight(event) {
+    selectFigure(event)
+}
+
+function possibleStepBishop(event) {
+    selectFigure(event)
+}
+
+function possibleStepQueen(event) {
+    selectFigure(event)
+}
+
+function possibleStepRookKing(event) {
+    selectFigure(event)
+}
+
+function possibleStepPawn(event) {
+    selectFigure(event)
 }
