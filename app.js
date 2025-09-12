50f33a0267564849aa60619bc034ada3b5a4b8ce
LinaLunatik <lina.rudnewa@yandex.ru>
2025-09-12 12:43:05 +0300

    Возможный ход ладьи
    При выборе ладьи подсвечивается возможный ход по горизонтали и вертикали, а также другим цветом выделена сама ладья. При повторном нажатии на ладью все подсветки отключаются.

--- app.js
+++ app.js
@@ -12,31 +12,44 @@ queens.forEach(queen => {queen.addEventListener('click', possibleStepQueen)})
 kings.forEach(king => {king.addEventListener('click', possibleStepRookKing)})
 pawns.forEach(pawn => {pawn.addEventListener('click', possibleStepPawn)})
 
-function selectFigure(event) {
-    const cell = event.target.parentElement;
-    cell.classList.toggle('selectedItem')
-}
-
 function possibleStepRook(event) {
-    selectFigure(event)
+    const cell = event.target.parentElement; //ячейка с ладьей
+    const raw = cell.parentElement;         //строка с ладьей
+    const td_raw = raw.querySelectorAll('td'); //все ячейки строки с ладьей
+    const rawIndex = Array.from(td_raw).indexOf(cell)
+    
+    cell.classList.toggle('selectedItem');
+
+    if (cell.classList.contains('selectedItem')) {
+        td_raw.forEach(td => {
+            if (td!==cell && !td.classList.contains('field-td')) 
+            {td.classList.add('possibleStep')}
+        });
+        
+        const td = document.querySelectorAll('tr')
+        td.forEach(raw => { 
+            if (!raw.classList.contains('field-tr')) {
+            raw.children[rawIndex].classList.add('possibleStep')}
+        }) 
+    } else {
+        td_raw.forEach(td => {if (td!==cell) {td.classList.remove('possibleStep')}});
+        document.querySelectorAll('tr').forEach(raw => {
+            raw.children[rawIndex].classList.remove('possibleStep')
+        })     
+    }
 }
 
 function possibleStepKnight(event) {
-    selectFigure(event)
 }
 
 function possibleStepBishop(event) {
-    selectFigure(event)
 }
 
 function possibleStepQueen(event) {
-    selectFigure(event)
 }
 
 function possibleStepRookKing(event) {
-    selectFigure(event)
 }
 
 function possibleStepPawn(event) {
-    selectFigure(event)
 }
