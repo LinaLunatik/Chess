4aca7ac33dd80ad082a2a90d2b832c9f29fa0dc2
LinaLunatik <lina.rudnewa@yandex.ru>
2025-09-15 14:39:10 +0300

    Возможный ход пешки
    При выделении пешки подсвечивается 2 клетки вниз от нее (если черная), и 2 клетки вверх от пешки (если белая). При повторном клике подсветка снимается

--- app.js
+++ app.js
@@ -52,4 +52,46 @@ function possibleStepRookKing(event) {
 }
 
 function possibleStepPawn(event) {
+    const cell = event.target.parentElement; //ячейка с пешкой
+    const row = cell.parentElement;         //строка с пешкой
+    const td_row = row.querySelectorAll('td'); //все ячейки строки с пешкой
+    const cellIndex = Array.from(td_row).indexOf(cell)
+    const rows = document.querySelectorAll('tr') //получаем все строки таблицы
+    const rowIndex = Array.from(rows).indexOf(row) //получаем индекс строки с пешкой
+
+    const wasSelected = cell.classList.contains('selectedItem')
+    cell.classList.toggle('selectedItem'); //выделение пешки
+
+    if (wasSelected) {
+        document.querySelectorAll('.possibleStep').forEach(el => 
+            {el.classList.remove('possibleStep')})
+    } else {
+        if (cell.classList.contains('selectedItem')) { 
+            if (cell.classList.contains('black')) {
+                BlackPawnSteps(rows, rowIndex, cellIndex)
+            } else if (cell.classList.contains('white')) {
+                WhitePawnSteps(rows, rowIndex, cellIndex)
+            }
+        }
+    }
+
+function BlackPawnSteps (rows, rowIndex, cellIndex) {
+    const next1RowBlack = rows[rowIndex + 1]
+    const next2RowBlack = rows[rowIndex + 2]
+    const next1CellBlack = next1RowBlack.children[cellIndex]
+    const next2CellBlack = next2RowBlack.children[cellIndex]
+    
+    next1CellBlack.classList.add('possibleStep')
+    next2CellBlack.classList.add('possibleStep')
+}
+
+function WhitePawnSteps (rows, rowIndex, cellIndex) {
+    const next1RowWhite = rows[rowIndex - 1]
+    const next2RowWhite = rows[rowIndex - 2]
+    const next1CellWhite = next1RowWhite.children[cellIndex]
+    const next2CellWhite = next2RowWhite.children[cellIndex]
+
+    next1CellWhite.classList.add('possibleStep')
+    next2CellWhite.classList.add('possibleStep')
+}
 }
