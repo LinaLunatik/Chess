99d08b4cd19b8cbc2b2cef22f1943c693896244e
LinaLunatik <lina.rudnewa@yandex.ru>
2025-09-16 13:56:39 +0300

    Исправление функции возможного хода ладьи
    Исправила ошибку в наименовании переменных в функции возможного хода ладьи

--- app.js
+++ app.js
@@ -13,28 +13,29 @@ kings.forEach(king => { king.addEventListener('click', possibleStepRookKing) })
 pawns.forEach(pawn => { pawn.addEventListener('click', possibleStepPawn) })
 
 function possibleStepRook(event) {
-    const cell = event.target.parentElement; //ячейка с ладьей
-    const raw = cell.parentElement;         //строка с ладьей
-    const td_raw = raw.querySelectorAll('td'); //все ячейки строки с ладьей
-    const rawIndex = Array.from(td_raw).indexOf(cell)
+    const cell = event.target.parentElement;
+    const row = cell.parentElement;
+    const td_row = row.querySelectorAll('td');
+    const rowIndex = Array.from(td_row).indexOf(cell)
 
     cell.classList.toggle('selectedItem');
 
     if (cell.classList.contains('selectedItem')) {
-        td_raw.forEach(td => {
-            if (td !== cell && !td.classList.contains('field-td')) { td.classList.add('possibleStep') }
+        td_row.forEach(td => {
+            if (td !== cell && !td.classList.contains('field-td')) { 
+                td.classList.add('possibleStep')}
         });
 
         const td = document.querySelectorAll('tr')
-        td.forEach(raw => {
-            if (!raw.classList.contains('field-tr')) {
-                raw.children[rawIndex].classList.add('possibleStep')
+        td.forEach(row => {
+            if (!row.classList.contains('field-tr')) {
+                row.children[rowIndex].classList.add('possibleStep')
             }
         })
     } else {
-        td_raw.forEach(td => { if (td !== cell) { td.classList.remove('possibleStep') } });
-        document.querySelectorAll('tr').forEach(raw => {
-            raw.children[rawIndex].classList.remove('possibleStep')
+        td_row.forEach(td => { if (td !== cell) { td.classList.remove('possibleStep') } });
+        document.querySelectorAll('tr').forEach(row => {
+            row.children[rowIndex].classList.remove('possibleStep')
         })
     }
 }
