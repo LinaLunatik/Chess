845066fe67b0013cb20b2284cdedb78a8f79d2e5
LinaLunatik <lina.rudnewa@yandex.ru>
2025-09-16 14:23:07 +0300

    Возможный ход ферзя
    При выделении ферзя подсвечиваются варианты хода. При повторном нажатии подсветка снимается

--- app.js
+++ app.js
@@ -197,6 +197,48 @@ function possibleStepBishop(event) {
 }
 
 function possibleStepQueen(event) {
+    const cell = event.target.parentElement; 
+    const row = cell.parentElement;         
+    const td_row = row.querySelectorAll('td'); 
+    const cellIndex = Array.from(td_row).indexOf(cell)
+    const rows = document.querySelectorAll('tr') 
+    const rowIndex = Array.from(rows).indexOf(row) 
+
+    const wasSelected = cell.classList.contains('selectedItem')
+    cell.classList.toggle('selectedItem'); 
+
+    if (wasSelected) {
+        document.querySelectorAll('.possibleStep').forEach(el => {el.classList.remove('possibleStep')})
+    } else {
+        if (cell.classList.contains('selectedItem')) {
+                QueenSteps(rows, rowIndex, cellIndex)
+    }}
+
+    function QueenSteps(rows, rowIndex, cellIndex) {
+        const directions = [
+            [-1, -1],
+            [-1, 1],
+            [1, -1],
+            [1, 1],
+            [-1, 0],
+            [0, -1],
+            [0, 1],
+            [1, 0]
+        ]
+
+        directions.forEach(([rowDir, cellDir]) => {
+            for (i = 1; i < 8; i++) {
+                const targetRow = rowIndex + rowDir * i;
+                const targetCell = cellIndex + cellDir * i;
+
+                if (targetRow >= 1 && targetRow <=8 && targetCell >= 1 && targetCell <= 8) {
+                    const targetCellElement = rows[targetRow].children[targetCell]
+                    targetCellElement.classList.add('possibleStep')
+                }
+            }
+        })
+    }
+
 }
 
 function possibleStepRookKing(event) {
