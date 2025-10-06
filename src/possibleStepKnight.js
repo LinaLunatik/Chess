54888c17807df6c6b82a40c51945a77112c5591b
LinaLunatik <lina.rudnewa@yandex.ru>
2025-10-06 11:22:28 +0300

    Оптимизировала функцию PossibleStepKnight
    Изменила расчет возможного хода: вместо отдельных функций на каждый вариант направления, создала массив из вариантов и одну функцию на ход

--- src/possibleStepKnight.js
+++ src/possibleStepKnight.js
@@ -6,107 +6,37 @@ export function possibleStepKnight(event) {
     const wasSelected = cell.classList.contains('selectedItem')
     cell.classList.toggle('selectedItem');
 
+    const KnightStep = (rows, rowIndex, cellIndex) => {
+        const directions = [
+            [2, 1],
+            [2, -1],
+            [1, -2],
+            [-1, -2],
+            [-2, -1],
+            [-2, 1],
+            [-1, 2],
+            [1, 2]
+        ]
+
+        directions.forEach(([rowDir, cellDir]) => {
+            const targetRow = rowIndex + rowDir;
+            const targetCell = cellIndex + cellDir;
+
+            if (targetRow >= 1 && 
+                targetRow <= 8 && 
+                targetCell >= 1 && 
+                targetCell <= 8) {
+                    const targetCellElement = rows[targetRow].children[targetCell]
+                    targetCellElement.classList.add('possibleStep')
+                }
+        })
+    }
+
     if (wasSelected) {
         document.querySelectorAll('.possibleStep').forEach(el => { el.classList.remove('possibleStep') })
     } else {
         if (cell.classList.contains('selectedItem')) {
-            KnightStep1(rows, rowIndex, cellIndex);
-            KnightStep2(rows, rowIndex, cellIndex);
-            KnightStep3(rows, rowIndex, cellIndex);
-            KnightStep4(rows, rowIndex, cellIndex);
-            KnightStep5(rows, rowIndex, cellIndex);
-            KnightStep6(rows, rowIndex, cellIndex);
-            KnightStep7(rows, rowIndex, cellIndex);
-            KnightStep8(rows, rowIndex, cellIndex)
-        }
-    }
-
-    function KnightStep1(rows, rowIndex, cellIndex) {
-        const targetRowIndex = rowIndex + 2
-        const targetCellIndex = cellIndex + 1
-
-        if (targetRowIndex >= 1 && targetRowIndex <= 8 &&
-            targetCellIndex >= 1 && targetCellIndex <= 8) {
-            const stepRow = rows[targetRowIndex]
-            const stepCell = stepRow.children[targetCellIndex]
-            stepCell.classList.add('possibleStep')
-        }
-    }
-    function KnightStep2(rows, rowIndex, cellIndex) {
-        const targetRowIndex = rowIndex + 2
-        const targetCellIndex = cellIndex - 1
-
-        if (targetRowIndex >= 1 && targetRowIndex <= 8 &&
-            targetCellIndex >= 1 && targetCellIndex <= 8) {
-            const stepRow = rows[targetRowIndex]
-            const stepCell = stepRow.children[targetCellIndex]
-            stepCell.classList.add('possibleStep')
-        }
-    }
-    function KnightStep3(rows, rowIndex, cellIndex) {
-        const targetRowIndex = rowIndex + 1
-        const targetCellIndex = cellIndex - 2
-
-        if (targetRowIndex >= 1 && targetRowIndex <= 8 &&
-            targetCellIndex >= 1 && targetCellIndex <= 8) {
-            const stepRow = rows[targetRowIndex]
-            const stepCell = stepRow.children[targetCellIndex]
-            stepCell.classList.add('possibleStep')
-        }
-    }
-    function KnightStep4(rows, rowIndex, cellIndex) {
-        const targetRowIndex = rowIndex - 1
-        const targetCellIndex = cellIndex - 2
-
-        if (targetRowIndex >= 1 && targetRowIndex <= 8 &&
-            targetCellIndex >= 1 && targetCellIndex <= 8) {
-            const stepRow = rows[targetRowIndex]
-            const stepCell = stepRow.children[targetCellIndex]
-            stepCell.classList.add('possibleStep')
-        }
-    }
-    function KnightStep5(rows, rowIndex, cellIndex) {
-        const targetRowIndex = rowIndex - 2
-        const targetCellIndex = cellIndex - 1
-
-        if (targetRowIndex >= 1 && targetRowIndex <= 8 &&
-            targetCellIndex >= 1 && targetCellIndex <= 8) {
-            const stepRow = rows[targetRowIndex]
-            const stepCell = stepRow.children[targetCellIndex]
-            stepCell.classList.add('possibleStep')
-        }
-    }
-    function KnightStep6(rows, rowIndex, cellIndex) {
-        const targetRowIndex = rowIndex - 2
-        const targetCellIndex = cellIndex + 1
-
-        if (targetRowIndex >= 1 && targetRowIndex <= 8 &&
-            targetCellIndex >= 1 && targetCellIndex <= 8) {
-            const stepRow = rows[targetRowIndex]
-            const stepCell = stepRow.children[targetCellIndex]
-            stepCell.classList.add('possibleStep')
-        }
-    }
-    function KnightStep7(rows, rowIndex, cellIndex) {
-        const targetRowIndex = rowIndex - 1
-        const targetCellIndex = cellIndex + 2
-
-        if (targetRowIndex >= 1 && targetRowIndex <= 8 &&
-            targetCellIndex >= 1 && targetCellIndex <= 8) {
-            const stepRow = rows[targetRowIndex]
-            const stepCell = stepRow.children[targetCellIndex]
-            stepCell.classList.add('possibleStep')
-        }
-    }
-    function KnightStep8(rows, rowIndex, cellIndex) {
-        const targetRowIndex = rowIndex + 1
-        const targetCellIndex = cellIndex + 2
-
-        if (targetRowIndex >= 1 && targetRowIndex <= 8 &&
-            targetCellIndex >= 1 && targetCellIndex <= 8) {
-            const stepRow = rows[targetRowIndex]
-            const stepCell = stepRow.children[targetCellIndex]
-            stepCell.classList.add('possibleStep')
+            KnightStep(rows, rowIndex, cellIndex)
         }
     }
 }
