0079527659b6edbd65c848aeaa1f499af9412fc6
LinaLunatik <lina.rudnewa@yandex.ru>
2025-10-17 14:02:48 +0300

    Создание модуля констант
    Созданы константы - объект с наименованием фигур и размер шахматной доски

---
+++ src/const.js
@@ -0,0 +1,11 @@
+export const CHESS_BOARD_SIZE = 8
+export const NAMES = {
+    rook: 'rook',
+    bishop: 'bishop',
+    knight: 'knight',
+    king: 'king',
+    queen: 'queen',
+    blackPawn: 'blackPawn',
+    whitePawn: 'whitePawn',
+    pawn: 'pawn'
+}
--- src/handleCellClick.js
+++ src/handleCellClick.js
@@ -1,9 +1,18 @@
 import { lightPossibleSteps } from './lightPossibleSteps.js'
+import { NAMES } from './const.js'
 
 export const handleCellClick = (event) => {
     const cell = event.currentTarget
 
-    const figures = ['rook', 'knight', 'bishop', 'queen', 'king', 'blackPawn', 'whitePawn']
+    const figures = [
+        NAMES.rook, 
+        NAMES.knight, 
+        NAMES.bishop, 
+        NAMES.queen, 
+        NAMES.king, 
+        NAMES.blackPawn,
+        NAMES.whitePawn
+    ]
 
     for (let figure of figures) {
         if (cell.querySelector('.' + figure)) {
