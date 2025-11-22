5b93870f4c47c10cf997cb48037b54141a86b046
LinaLunatik <lina.rudnewa@yandex.ru>
2025-11-22 16:42:56 +0300

    Аналогично ладье исправлены функции остальных фигур

--- src/possibleStepsForFigures/kingSteps.js
+++ src/possibleStepsForFigures/kingSteps.js
@@ -1,25 +1,34 @@
 import { isOnChessBoard } from "/src/isOnChessBoard.js"
-import { STYLES } from "../const.js"
 
-export const kingSteps = (rows, rowIndex, cellIndex) => {
-        const directions = [
-            [-1, -1],
-            [-1, 1],
-            [1, -1],
-            [1, 1],
-            [-1, 0],
-            [0, -1],
-            [0, 1],
-            [1, 0]
-        ]
+export const kingSteps = (state, row, col) => {
+    const directions = [
+        [-1, -1],
+        [-1, 1],
+        [1, -1],
+        [1, 1],
+        [-1, 0],
+        [0, -1],
+        [0, 1],
+        [1, 0]
+    ]
 
-        directions.forEach(([rowDir, cellDir]) => {
-            const targetRow = rowIndex + rowDir;
-            const targetCell = cellIndex + cellDir;
+    let moves = []
+    const currentColor = state.board[row][col][0]
 
-            if (isOnChessBoard(targetRow, targetCell)) {
-                const targetCellElement = rows[targetRow].children[targetCell]
-                targetCellElement.classList.add(STYLES.possibleStep)
+    directions.forEach(([rowDir, colDir]) => {
+        const targetRow = row + rowDir;
+        const targetCol = col + colDir;
+
+        if (isOnChessBoard(targetRow, targetCol)) {
+            const targetCellFigure = state.board[targetRow][targetCol]
+            if (targetCellFigure === '') { //если клетка пуста, можно идти
+                moves.push({ row: targetRow, col: targetCol, type: 'step' })
             }
-        })
-    }
+            else if (targetCellFigure[0] !== currentColor) {
+                moves.push({ row: targetRow, col: targetCol, type: 'capture' })
+                //если клетка занята фигурой чужого цвета, съесть, потом стоп
+            }
+        }
+    })
+    return moves
+}
