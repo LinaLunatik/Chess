6b8af69272193dba8d7345c6e9520fe028eba405
LinaLunatik <LinaLunatik@users.noreply.github.com>
2025-12-04 13:36:27 +0000

    deploy: 4a184387c5a599d09742cf4e78f298a861713c08

--- Separation/renderCell.js
+++ Separation/renderCell.js
@@ -1,24 +1,24 @@
-import { FIGURE_IMAGE_PATH, TYPE_TO_CLASS, STYLES } from "./src/const.js";
+import { FIGURE_IMAGE_PATH, STYLES } from "./src/const.js";
 
-export const renderCell = (figure, row, col, { isSelected, isPossibleStep }) => {
+export const renderCell = (cell, { isSelected, isPossibleStep }) => {
 
+    const color = cell.isBlack === null ? null : (cell.isBlack ? 'black' : 'white')
     let imgTag = ''
 
-    if (figure && figure.length === 2) {
-        const color = figure[0];
-        const type = figure[1];
-
-        let className = TYPE_TO_CLASS[type];
-        if (type === 'P') { className = color === 'b' ? 'blackPawn' : 'whitePawn' }
-
-        const src = FIGURE_IMAGE_PATH[color][type];
-
-        imgTag = `<img src="${src}" alt="${figure}" class="${className}">`;
+    if (cell.figure && color) {
+        const src = FIGURE_IMAGE_PATH[cell.figure][color];
+        if (src) {
+            imgTag = `<img src="${src}" alt="${cell.figure}" class="${cell.figure}">`;
+        }
     }
 
     let cellClass = 'cell'
     if (isSelected) { cellClass += ' ' + STYLES.selectedItem };
     if (isPossibleStep) { cellClass += ' ' + STYLES.possibleStep };
 
-    return `<td data-row="${row}" data-col="${col}" class="${cellClass}">${imgTag}</td>`
+    return `<td data-row="${cell.row}" 
+                data-col="${cell.col}" 
+                class="${cellClass}">
+                ${imgTag}
+            </td>`
 }
