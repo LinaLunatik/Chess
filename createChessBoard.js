35c264dc71c7c08626275e6a0f65f3f0a1727dc0
LinaLunatik <lina.rudnewa@yandex.ru>
2025-12-29 15:01:33 +0300

    изменила логику в handleCellClick
    Изменила логику в handleCellClick с учетом получения данных из состояния , а не DOM, поэтому ф-я getCoordsOfCell  больше не нужна

--- createChessBoard.js
+++ createChessBoard.js
@@ -1,10 +1,11 @@
-import { initialState } from "./state.js"
+import { getState } from "./state.js"
 import { buildChessBoardHTML } from "./buildChessBoardHTML.js"
 import { displayChessBoard } from "./displayChessBoard.js"
 import { attachEventListeners } from "./attachEventListeners.js"
 
 export const createChessBoard = () => {
-    const html = buildChessBoardHTML(initialState);
+    const currentState = getState();
+    const html = buildChessBoardHTML(currentState);
     displayChessBoard(html);
     attachEventListeners();
 }
--- src/handleCellClick.js
+++ src/handleCellClick.js
@@ -1,5 +1,4 @@
-import { FIGURES, TYPE_TO_CLASS, possibleStepsMap } from './const.js'
-import { getCoordsOfCell } from './getCoordsOfCell.js'
+import { FIGURES, possibleStepsMap } from './const.js'
 import { getState, setState } from '../state.js'
 import { buildChessBoardHTML } from '../buildChessBoardHTML.js'
 import { displayChessBoard } from '../displayChessBoard.js'
@@ -8,17 +7,18 @@ import { moveFigure } from '../moveFigure.js'
 
 export const handleCellClick = (cell) => {
     const currentState = getState()
-    const coords = getCoordsOfCell(cell);  // ← получаем объект
-    const { colIndex: col, rowIndex: row } = coords; // Теперь деструктуризация
-    const figure = currentState.board[row][col]; //получаем фигуру из состояния
+    
+    //получаем фигуру из состояния
+    const figure = cell.figure 
+    const {row, col} = cell
 
-    if (figure && figure.length === 2) { //определяем фигуру
-
+    if (figure) {
         const isSameFigure = currentState.selectedCell?.row === row &&
             currentState.selectedCell?.col === col;
 
-        if (isSameFigure) { //клик по той же фигуре
-            const newState = { //сброс
+        //если клик по той же фигуре, то сброс
+        if (isSameFigure) { 
+            const newState = {
                 ...currentState,
                 selectedCell: null,
                 possibleSteps: []
@@ -28,21 +28,22 @@ export const handleCellClick = (cell) => {
             const html = buildChessBoardHTML(newState)
             displayChessBoard(html)
             attachEventListeners()
-        } else { //клик по новой фигуре - выбор новой фигуры
-            const color = figure[0]
-            const type = figure[1]
-
-            let typeName = TYPE_TO_CLASS[type]
-            if (type === 'P') { 
-                typeName = color === 'b' ? FIGURES.blackPawn : FIGURES.whitePawn }
-
-            const getSteps = possibleStepsMap[typeName] //берем фунцию расчет хода фигуры
+        } 
+        
+        //если клик по новой фигуре - выбор новой фигуры
+        else { 
+            //получаем фунцию расчет хода фигуры
+            const getSteps = possibleStepsMap[figure] 
 
             if (getSteps) {
-                const steps = getSteps(currentState, row, col) //вызываем функцию расчета хода
+                //вызываем функцию расчета хода
+                const steps = getSteps(currentState, row, col) 
 
-                if (!Array.isArray(steps)) { return }
-
+                const newState = {
+                    ...currentState,
+                    selectedCell: {row, col},
+                    possibleSteps: steps
+                }
               
                 setState(newState)
                 const html = buildChessBoardHTML(newState)
