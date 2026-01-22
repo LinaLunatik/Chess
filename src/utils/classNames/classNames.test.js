5d9aabe6f1dbf793ba9792d63690cec829f042cd
LinaLunatik <lina.rudnewa@yandex.ru>
2026-01-22 14:00:34 +0300

    Создана своя ф-я classNames

---
+++ src/utils/classNames/classNames.test.js
@@ -0,0 +1,41 @@
+import { classNames } from "./classNames.js";
+const testCases = [
+    {
+        params: ['foo', 'bar'],
+        expect: 'foo bar',
+    },
+    {
+        params: ['foo', { bar: true }],
+        expect: 'foo bar',
+    },
+    {
+        params: [{ 'foo-bar': true }],
+        expect: 'foo-bar',
+    },
+    {
+        params: [{ 'foo-bar': false }],
+        expect: '',
+    },
+    {
+        params: [{ foo: true }, { bar: true }],
+        expect: 'foo bar',
+    },
+    {
+        params: [{ foo: true, bar: true }],
+        expect: 'foo bar',
+    },
+    {
+        params: ['foo', { bar: true, duck: false }, 'baz', { quux: true }],
+        expect: 'foo bar baz quux',
+    },
+    {
+        params: [null, false, 'bar', undefined, 0, 1, { baz: null }, ''],
+        expect: 'bar 1',
+    },
+]
+
+testCases.forEach(({params, expect}, index) => {
+    const result = classNames(...params)
+    console.log(`${index} ${result === expect ? 
+        '\\/' : `    Х result: '${result}' but expected '${expect}'`}`)
+});
