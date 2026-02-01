cec4b88345c63d100fb064322532a976cc521790
LinaLunatik <lina.rudnewa@yandex.ru>
2026-02-01 12:24:10 +0300

    classNames переделан на метод reduce

--- src/utils/classNames/classNames.js
+++ src/utils/classNames/classNames.js
@@ -1,20 +1,23 @@
 export const classNames = (...params) => {
-    let result = []
-    params.forEach(param => {
-        if (
-            param !== null &&
-            typeof param !== 'object' &&
-            typeof param !== 'function') {
-            if (param) { result.push(param) }
-        } else if (
-            param !== null &&
-            typeof param === 'object'
-        ) {
-            for (let key of Object.keys(param)) {
-                if (param[key]) { result.push(key) }
+    
+    let result = params.reduce((acc, param) => {
+        if (param !== null) {
+            if (
+                typeof param !== 'object' &&
+                typeof param !== 'function') {
+                if (param) {
+                    acc.push(param)
+                }
+            } else if (
+                typeof param === 'object'
+            ) {
+                for (let key of Object.keys(param)) {
+                    if (param[key]) {
+                        acc.push(key)
+                    }
+                }
             }
-        }
-    })
+        } return acc
+    }, [])
     return result.join(' ')
 }
-//params - массив бесконечный
