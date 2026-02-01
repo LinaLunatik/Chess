7abc915e8785b463309e13eac17ca08567d46a38
LinaLunatik <lina.rudnewa@yandex.ru>
2026-02-01 12:48:51 +0300

    сформировано md описание и расставлены комментарии

--- src/utils/classNames/classNames.js
+++ src/utils/classNames/classNames.js
@@ -2,16 +2,23 @@ export const classNames = (...params) => {
     
     let result = params.reduce((acc, param) => {
         if (param !== null) {
+            
+            // если param - примитив
             if (
                 typeof param !== 'object' &&
                 typeof param !== 'function') {
-                if (param) {
+                // сортируем только truthy значения 
+                    if (param) {
                     acc.push(param)
                 }
-            } else if (
+            } 
+            
+            // если param - объект
+            else if (
                 typeof param === 'object'
             ) {
                 for (let key of Object.keys(param)) {
+                    //сортируем только truthy ключи объекта
                     if (param[key]) {
                         acc.push(key)
                     }
