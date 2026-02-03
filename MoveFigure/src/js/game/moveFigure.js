82922a9b9a9affcd25f868006d825e4288f500a4
LinaLunatik <LinaLunatik@users.noreply.github.com>
2026-02-03 11:40:31 +0000

    deploy: 857700f079711c39b0206b505dcd2f323aeac833

--- MoveFigure/src/js/game/moveFigure.js
+++ MoveFigure/src/js/game/moveFigure.js
@@ -1,11 +1,42 @@
 import { getState, setState } from "./state.js"
+import { buildChessBoardHTML } from './buildChessBoardHTML.js'
+import { displayChessBoard } from "./displayChessBoard.js"
+import { attachEventListeners } from "./attachEventListeners.js"
 
-export const moveFigure = (targetRow, targetCol) => {
+export const moveFigure = (row, col) => {
+    const currentState = getState()
+
+    if (!currentState.selectedCell) {
+        console.error('Нет выбранной клетки');
+        return;
+    }
+
+    const { row: fromRow, col: fromCol } = currentState.selectedCell
+
+    const newBoard = currentState.board.map(
+        row => row.map(
+            cell => ({ ...cell })
+        ))
+
+    const currentCell = newBoard[row][col]
+    const fromCell = newBoard[fromRow][fromCol]
+
+    currentCell.figure = fromCell.figure
+    currentCell.isBlack = fromCell.isBlack
+
+    fromCell.figure = null
+    fromCell.isBlack = null
 
     const newState = {
         ...currentState,
-        selectedFigure: { row, col },
-        possibleSteps: steps
+        board: newBoard,
+        selectedCell: null,
+        possibleSteps: []
     }
-    return newState
+    
+    setState(newState)
+
+    const html = buildChessBoardHTML(newState)
+                displayChessBoard(html)
+                attachEventListeners()
 }
