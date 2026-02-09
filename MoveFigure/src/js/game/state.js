f3a4f33dadf75e65b7396b899d61eb9aad4eee1a
LinaLunatik <LinaLunatik@users.noreply.github.com>
2026-02-09 11:32:24 +0000

    deploy: 4507de16c78cb9867cb72257cf80c06e30e4ecbc

--- MoveFigure/src/js/game/state.js
+++ MoveFigure/src/js/game/state.js
@@ -6,3 +6,10 @@ export const getState = () => state
 export const setState = (newState) => { state = newState } 
 
 export const getCell = ({row, col}) => state.board[row][col]
+export const setCell = (cell, fromCell) => {
+    return {
+        ...cell,
+        figure: fromCell.figure,
+        isBlack: fromCell.isBlack
+    }
+}
