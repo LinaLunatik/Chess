a918f67a9b038352c33ddc384bdc9a8c98085331
AlinaSaksonova <lina.rudnewa@yandex.ru>
2026-02-10 14:37:07 +0300

    Функция classNames переделанная (#7)
    * сформировано md описание и расставлены комментарии
    
    * упростила условия

--- src/utils/classNames/classNames.js
+++ src/utils/classNames/classNames.js
@@ -1,17 +1,24 @@
 export const classNames = (...params) => {
-    
+
     let result = params.reduce((acc, param) => {
-        if (param !== null) {
-            if (
+        // сортируем только truthy значения
+        if (param) {
+            // если param - примитив
+            if
+            (
                 typeof param !== 'object' &&
-                typeof param !== 'function') {
-                if (param) {
-                    acc.push(param)
-                }
-            } else if (
+                typeof param !== 'function'
+            ) {
+                acc.push(param) 
+            }
+
+            // если param - объект
+            else if 
+            (
                 typeof param === 'object'
             ) {
                 for (let key of Object.keys(param)) {
+                    //сортируем только truthy ключи объекта
                     if (param[key]) {
                         acc.push(key)
                     }
