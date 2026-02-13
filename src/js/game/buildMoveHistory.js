87ae530a290abb5ec3463a5360550313d0d13347
LinaLunatik <lina.rudnewa@yandex.ru>
2026-02-13 15:57:27 +0300

    план отображения объектов

--- src/js/game/buildMoveHistory.js
+++ src/js/game/buildMoveHistory.js
@@ -1,3 +1,27 @@
-export const buildMoveHistory = (state) => {
+import { STYLES } from "../const.js"
 
+export const buildMoveHistory = (state) => {
+    moves = state.moveHistory
+        //есть массив объектов 
+        // [{
+        // figure: figure,
+        // targetCell: {row, col},
+        // type: 'move' | 'capture' | 'castling' | 'check' | 'checkmate'
+        // }]
+    
+        // хочу фигуры отображать картинками
+        // targetCell надо изменить с индексов на координаты шахматные
+        // 'move' = ' '
+        // 'capture' = x
+        // 'castling' = 0-0
+        // 'check' = +
+        // 'checkmate' = #
+
+
+
+    return  `
+                <div class="${STYLES.moveHistory}">
+                    <div></div>
+                </div>
+            `
 }
