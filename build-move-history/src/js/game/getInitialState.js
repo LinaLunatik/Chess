fc35fc7e884ccf704da73b8d508d3e4b3a5143b5
LinaLunatik <LinaLunatik@users.noreply.github.com>
2026-02-13 11:21:14 +0000

    deploy: f8b0ebcac3d8ac709507985b1433e5af63bdf301

---
+++ build-move-history/src/js/game/getInitialState.js
@@ -0,0 +1,86 @@
+import { CHESS_BOARD_SIZE, FIGURES } from "../const.js"
+
+export const getInitialState = () => {
+
+    const createEmptyCell = () => ({
+        row: undefined,
+        col: undefined,
+        figure: null,
+        isBlack: null,
+        // TODO добавить подсветку selected | possibleStep | possibleCapture | null
+        highlight: null
+    })
+
+    const orderFirstLine = [
+        FIGURES.rook,
+        FIGURES.knight,
+        FIGURES.bishop,
+        FIGURES.queen,
+        FIGURES.king,
+        FIGURES.bishop,
+        FIGURES.knight,
+        FIGURES.rook
+    ]
+
+    const createCell = ({ figure, isBlack }) => (
+        { ...createEmptyCell(), 
+            figure, 
+            isBlack 
+        })
+
+    const firstBlackLine = orderFirstLine.map((figureType) => (
+        createCell({ figure: figureType, isBlack: true })))
+
+    const secondBlackLine = Array.from({ length: CHESS_BOARD_SIZE }, () => (
+        createCell({ figure: FIGURES.blackPawn, isBlack: true })))
+
+    const createEmptyRow = () => 
+        Array.from({ length: CHESS_BOARD_SIZE }, () => (
+        createEmptyCell()))
+
+    const firstWhiteLine = orderFirstLine.map((figureType) => (
+        createCell({ figure: figureType, isBlack: false })))
+
+    const secondWhiteLine = Array.from({ length: CHESS_BOARD_SIZE }, () => (
+        createCell({ figure: FIGURES.whitePawn, isBlack: false })))
+
+    const initialBoardStructure = [
+        firstBlackLine,
+        secondBlackLine,
+        createEmptyRow(),
+        createEmptyRow(),
+        createEmptyRow(),
+        createEmptyRow(),
+        secondWhiteLine,
+        firstWhiteLine
+    ]
+
+    //присваивание координат (row,col) каждой ячейке
+    const board = initialBoardStructure.map((row, rowIndex) =>
+        row.map((cell, colIndex) => (
+            { ...cell, row: rowIndex, col: colIndex }
+        )))
+
+    const initialState = {
+        board,
+        // true or false
+        isCurrentPlayerWhite: true,
+        // {row, col} | null,
+        selectedCell: null,
+        // [{row, col, type: 'step' | 'capture'}]
+        possibleSteps: [],
+        // съеденные фигуры / строки
+        capturedFigures: {
+            white: [],
+            black: []
+        },
+        // [{
+        // figure: figure,
+        // targetCell: {row, col},
+        // type: 'move' | 'capture' | 'castling' | 'check' | 'checkmate'
+        // }]
+        moveHistory: []
+    }
+
+    return initialState
+}
---
+++ build-move-history/src/js/game/state.js
@@ -0,0 +1,63 @@
+import { getInitialState } from "./getInitialState.js"
+
+let state = getInitialState()
+
+export const getState = () => state
+export const setState = (newState) => { state = newState } 
+
+export const getCell = ({row, col}) => state.board[row][col]
+export const setCell = (cell, fromCell) => {
+    return {
+        ...cell,
+        figure: fromCell.figure,
+        isBlack: fromCell.isBlack
+    }
+}
+
+export const setBoard = (newBoard)  => {
+    const state = getState()
+    setState({
+        ...state,
+        board: newBoard
+    })
+}
+
+export const clearSelectedCell = () => {
+    const state = getState()
+    setState({
+        ...state,
+        selectedCell: null
+    })
+}
+
+export const setSelectedCell = (cell) => {
+    const state = getState()
+    setState({
+        ...state,
+        selectedCell: cell
+    })
+}
+
+export const clearPossibleSteps = () => {
+    const state = getState()
+    setState({
+        ...state,
+        possibleSteps: []
+    })
+}
+
+export const setPossibleSteps = (steps) => {
+    const state = getState()
+    setState({
+        ...state,
+        possibleSteps: steps
+    })
+}
+
+export const setCapturedFigures = (newCapturedFigures) => {
+    const state = getState()
+    setState({
+        ...state,
+        capturedFigures: newCapturedFigures
+    })
+}
