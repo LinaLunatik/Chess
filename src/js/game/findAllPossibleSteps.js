196882ecee79f8159014ae8f4667aa7037b552d1
LinaLunatik <lina.rudnewa@yandex.ru>
2026-02-26 12:09:55 +0300

    ф-я findAllPossibleSteps ищет возможные ходы для массива фигур

--- src/js/game/findAllPossibleSteps.js
+++ src/js/game/findAllPossibleSteps.js
@@ -1,17 +1,32 @@
-import { possibleStepsMap } from "../const.js"
+import { FIGURES, possibleStepsMap } from "../const.js"
+import { getValidStepsKing } from "./getValidStepsKing.js"
 
 export const findAllPossibleSteps = (state, figures) => {
-        
+
     const allSteps = []
-    
-        for (let figure of figures) {
-            const { figure: figureType, row, col } = figure
+
+    for (let figure of figures) {
+        const { figure: figureType, row, col } = figure
+        //проверка шагов короля на шах
+        if (figureType === FIGURES.king) {
+
+            const getKingSteps = possibleStepsMap[FIGURES.king]
+            const stepsKing = getKingSteps(state, row, col)
+            const kingCell = {row, col}
+
+            const validStepsKing = getValidStepsKing(state, stepsKing, kingCell)
+
+            allSteps.push(...validStepsKing)
+
+        } else {
+            //остальные фигуры      
             const getSteps = possibleStepsMap[figureType]
-    
+
             if (getSteps) {
                 const steps = getSteps(state, row, col)
                 allSteps.push(...steps)
             }
         }
+    }
     return allSteps
 }
