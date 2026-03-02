19c955b62e1e624de352d78dd84050d7460a2cf9
LinaLunatik <LinaLunatik@users.noreply.github.com>
2026-03-02 09:08:37 +0000

    deploy: 10708feb7f8357052e0ae49c28629b79cbfae948

---
+++ pawn-promotion/src/js/game/buildChessBoardHTML.js
@@ -0,0 +1,29 @@
+import { renderCell } from "./renderCell.js"
+import { STYLES } from "../const.js"
+
+export const buildChessBoardHTML = (state) => {
+
+    const board = state.board
+    const targetCells = new Set(
+        state.possibleSteps.map(step => `${step.row},${step.col}`)
+    );
+
+    const trElements= board.map(row => {
+        const tdElements = row.map(cell => {
+            const isSelected = 
+                state.selectedCell?.row === cell.row &&
+                state.selectedCell?.col === cell.col;
+            const isPossibleStep = targetCells.has(`${cell.row},${cell.col}`);
+
+            return renderCell(cell, { isSelected, isPossibleStep })
+        });
+        return `<tr>
+                    ${tdElements.join('')}
+                </tr>`;
+    })
+
+    return `<table 
+                class="${STYLES.table}">
+                    ${trElements.join('')}
+            </table>`
+}
---
+++ pawn-promotion/src/js/game/moveFigure.js
@@ -0,0 +1,71 @@
+import {
+    clearPossibleSteps,
+    clearSelectedCell,
+    getState,
+    setBoard,
+    setCapturedFigures,
+    setCell
+} from "./state.js"
+import { createChessBoard } from "./createChessBoard.js"
+import { clearCell } from "./clearCell.js"
+import { toggleCurrentPlayer } from "./toggleCurrentPlayer.js"
+import { OPPOSITE_COLORS } from "../const.js"
+import { isItEndGame } from "./isItEndGame.js"
+
+export const moveFigure = (cell) => {
+    const { row, col } = cell
+    const state = getState()
+
+    if (!state.selectedCell) {
+        console.error('Нет выбранной клетки');
+        return;
+    }
+
+    const { row: fromRow, col: fromCol } = state.selectedCell
+
+    const newBoard = state.board.map(
+        row => row.map(
+            cell => ({ ...cell })
+        ))
+
+    const targetCell = newBoard[row][col]
+    const fromCell = newBoard[fromRow][fromCol]
+    const opponentColor = OPPOSITE_COLORS[fromCell.color]
+
+    const newCapturedFigures = {
+        ...state.capturedFigures,
+        black: [...state.capturedFigures.black],
+        white: [...state.capturedFigures.white]
+    }
+
+    //если клетка назначения занята чужой фигурой
+    if (targetCell.figure !== null) {
+        newCapturedFigures[targetCell.color].push(targetCell.figure)
+    }
+
+    newBoard[row][col] = setCell(targetCell, fromCell)
+
+    clearCell(fromCell)
+
+    setBoard(newBoard)
+    clearSelectedCell()
+    clearPossibleSteps()
+    setCapturedFigures(newCapturedFigures)
+
+    const newState = getState()
+    const resultGame = isItEndGame(newState, opponentColor)
+
+    if (resultGame.isItStalemate) {
+        alert('ПАТ')
+    } else if (resultGame.isItCheckmate) {
+        alert('МАТ')
+    } else {
+        // игра продолжается
+        if (resultGame.isCheck) {
+            //здесь будет подключаться подсветка короля под шахом
+            console.log('ШАХ')
+        }
+        toggleCurrentPlayer()
+        createChessBoard()
+    }
+}
