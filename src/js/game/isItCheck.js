8ee1e080df88acd315fe3bc69c3e1fccf1ba90bb
LinaLunatik <lina.rudnewa@yandex.ru>
2026-03-03 14:06:15 +0300

    исправлено определение Пата

--- src/js/game/isItCheck.js
+++ src/js/game/isItCheck.js
@@ -1,16 +1,16 @@
 import { findKingCell } from "./findKingCell.js"
 import { findAllFiguresByColor } from "./findAllFiguresByColor.js"
-import { findAllPossibleSteps } from "./findAllPossibleSteps.js"
-import { COLORS } from "../const.js"
+import { OPPOSITE_COLORS } from "../const.js"
+import { findAllGeometricSteps } from "./findAllGeometricSteps.js"
 
 export const isItCheck = (state, colorOfKing) => {
 
     const kingCell = findKingCell(state, colorOfKing)
     if (!kingCell) return false
 
-    const opponentColor = colorOfKing === COLORS.BLACK ? COLORS.WHITE : COLORS.BLACK
+    const opponentColor = OPPOSITE_COLORS[colorOfKing]
     const allOpponentFigures = findAllFiguresByColor(state, opponentColor)
-    const allSteps = findAllPossibleSteps(state, allOpponentFigures)
+    const allSteps = findAllGeometricSteps(state, allOpponentFigures)
 
     const isKingInDanger = allSteps.some(step =>
         step.row === kingCell.row &&
