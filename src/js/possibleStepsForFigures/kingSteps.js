5a90b6b7e2cd67918c33282b60cc9d9185b3499c
LinaLunatik <lina.rudnewa@yandex.ru>
2026-03-30 14:06:40 +0300

    добавила рокировку в расчет возможных ходов короля

--- src/js/possibleStepsForFigures/kingSteps.js
+++ src/js/possibleStepsForFigures/kingSteps.js
@@ -1,4 +1,5 @@
-import { MOVE_TYPES } from "../const.js"
+import { MOVE_TYPES, ROOK_SIDE } from "../const.js"
+import { isCastlingValid } from "../game/isCastlingValid.js"
 import { isOnChessBoard } from "../game/isOnChessBoard.js"
 import { getCell } from "../game/state.js"
 
@@ -15,7 +16,7 @@ export const kingSteps = (state, row, col) => {
     ]
 
     let moves = []
-    const { figure, color: currentColor } = getCell({row, col})
+    const { figure, color: currentColor } = getCell({ row, col })
 
     if (!figure) return []
 
@@ -24,27 +25,56 @@ export const kingSteps = (state, row, col) => {
         const targetCol = col + colDir;
 
         if (isOnChessBoard(targetRow, targetCol)) {
-            const targetCell = getCell({row: targetRow, col: targetCol})
-            const {figure: targetFigure, color: targetColor} = targetCell
-            
+            const targetCell = getCell({ row: targetRow, col: targetCol })
+            const { figure: targetFigure, color: targetColor } = targetCell
+
             //если клетка пуста, можно идти
             if (targetFigure === null) {
-                moves.push({ 
-                    row: targetRow, 
-                    col: targetCol, 
-                    type: MOVE_TYPES.step 
+                moves.push({
+                    row: targetRow,
+                    col: targetCol,
+                    type: MOVE_TYPES.step
                 })
             }
-            
+
             //если клетка занята фигурой чужого цвета, съесть, потом стоп
             else if (targetColor !== currentColor) {
-                moves.push({ 
-                    row: targetRow, 
-                    col: targetCol, 
-                    type: MOVE_TYPES.capture 
+                moves.push({
+                    row: targetRow,
+                    col: targetCol,
+                    type: MOVE_TYPES.capture
                 })
             }
         }
     })
+    // Короткая рокировка
+    if (state.castlingRights[currentColor][ROOK_SIDE.kingSide]) {
+        const canCastle = isCastlingValid(state, currentColor, ROOK_SIDE.kingSide)
+        if (canCastle) {
+            moves.push({
+                row: row,
+                col: (col + 2),
+                type: MOVE_TYPES.castling,
+                rookSide: ROOK_SIDE.kingSide,
+                rookFrom: { row: row, col: (col + 3) },
+                rookTo: { row: row, col: (col + 1) }
+            })
+        }
+    }
+    // Длинная рокировка
+    if (state.castlingRights[currentColor][ROOK_SIDE.queenSide]) {
+        const canCastle = isCastlingValid(state, currentColor, ROOK_SIDE.queenSide)
+        if (canCastle) {
+            moves.push({
+                row: row,
+                col: (col - 2),
+                type: MOVE_TYPES.castling,
+                rookSide: ROOK_SIDE.queenSide,
+                rookFrom: { row: row, col: (col - 4) },
+                rookTo: { row: row, col: (col - 1) }
+
+            })
+        }
+    }
     return moves
 }
