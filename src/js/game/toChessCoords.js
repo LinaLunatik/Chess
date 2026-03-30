9dad111fd77dfd827c3e18d2d54b8613a108eb88
LinaLunatik <lina.rudnewa@yandex.ru>
2026-03-30 15:00:39 +0300

    добавлена константа и автоматический расчет последовательности букв из ASCII_CODE

--- src/js/game/toChessCoords.js
+++ src/js/game/toChessCoords.js
@@ -1,7 +1,9 @@
-import { CHESS_BOARD_SIZE } from "../const.js"
+import { ASCII_CODE_A, CHESS_BOARD_SIZE } from "../const.js"
+
+const cols = String.fromCharCode(...[...Array(CHESS_BOARD_SIZE)]
+    .map((_, i) => ASCII_CODE_A + i))
 
 export const toChessCoords = (row, col) => {
-    const cols = 'abcdefgh'
     const letter = cols[col]
     const number = CHESS_BOARD_SIZE - row
 
