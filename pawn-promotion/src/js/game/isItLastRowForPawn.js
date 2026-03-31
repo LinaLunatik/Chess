713ae49d81e8d21452815bf6e641874635af76d7
LinaLunatik <LinaLunatik@users.noreply.github.com>
2026-03-31 11:37:16 +0000

    deploy: 4380d3743a3cdda830938ee0f82582f74f23575a

--- pawn-promotion/src/js/game/isItLastRowForPawn.js
+++ pawn-promotion/src/js/game/isItLastRowForPawn.js
@@ -1,8 +1,7 @@
-import { FIGURES } from "../const.js"
+import { CHESS_BOARD_SIZE, FIGURES } from "../const.js"
 
-export const isItLastRowForPawn = (figure, row) => {
-    return (
-        (figure === FIGURES.whitePawn && row === 0) ||
-        (figure === FIGURES.blackPawn && row === 7)
-    )
-}
+export const isItLastRowForPawn = (figure, row) =>
+
+    (figure === FIGURES.whitePawn && row === 0) ||
+    (figure === FIGURES.blackPawn && row === (CHESS_BOARD_SIZE - 1))
+
