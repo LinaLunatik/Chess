1a35444695c898068a98d0c01f9a8f2828d98403
LinaLunatik <lina.rudnewa@yandex.ru>
2026-04-21 12:07:22 +0300

    добавлена константа и описание в getRookSide

--- src/js/game/getRookSide.js
+++ src/js/game/getRookSide.js
@@ -1,11 +1,19 @@
-import { CHESS_BOARD_SIZE } from "../const.js"
+import { CHESS_BOARD_SIZE, ROOK_SIDE } from "../const.js"
+
+/**
+ * Определяет сторону доски (королевская или ферзевая) по столбцу ладьи.
+ * Возвращает null, если ладья стоит не в крайнем столбце.
+ * 
+ * @param {number} col - Координата столбца (от 0 до 7)
+ * @returns {'queenSide' | 'kingSide' | null} - Название стороны доски или null
+ */
 
 export const getRookSide = (col) => {
     if (col === 0) {
-        return 'queenSide'
+        return ROOK_SIDE.queenSide
     }
     if (col === (CHESS_BOARD_SIZE - 1)) {
-        return 'kingSide'
+        return ROOK_SIDE.kingSide
     }
     return null
 }
